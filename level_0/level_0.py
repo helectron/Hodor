a515dba31ca8e7d54afa0be460949e8c4d30a32b
helectron <helena.cortes@udea.edu.co>
2021-05-26 22:16:01 -0500

    pep8 white line

--- level_0/level_0.py
+++ level_0/level_0.py
@@ -3,13 +3,13 @@
 # Import module
 import requests
 
-# Level 1
-times = 1025
+# Level 0
+times = 5
 my_id = {
     'id': '2282',
     'holdthedor': 'submit'
 }
-URL = 'http://158.69.76.135/level0.php'
+url = 'http://158.69.76.135/level0.php'
 
 for i in range(times):
-    get_response = requests.post(URL, data=my_id)
+    get_response = requests.post(url, data = my_id)
