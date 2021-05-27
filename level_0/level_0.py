c122b17e8c8fd7738a2b57b779258c278df91c27
helectron <helena.cortes@udea.edu.co>
2021-05-27 06:25:59 -0500

    learning door correctly

--- level_0/level_0.py
+++ level_0/level_0.py
@@ -4,12 +4,9 @@
 import requests
 
 # Level 0
-times = 5
-my_id = {
-    'id': '2282',
-    'holdthedor': 'submit'
-}
+times = 1025
+my_id = {'id': 2282,'holdthedoor': 'submit'}
 url = 'http://158.69.76.135/level0.php'
 
 for i in range(times):
-    get_response = requests.post(url, data = my_id)
+    get_response = requests.post(url, data=my_id)
