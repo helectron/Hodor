81cf71c1f011af58af651c37708c6660a2a4a490
helectron <helena.cortes@udea.edu.co>
2021-06-06 19:31:20 -0500

    Tim Britton's special

--- level_5/level_5.py
+++ level_5/level_5.py
@@ -10,9 +10,9 @@ except ImportError:
 
 URL = 'http://158.69.76.135/level5.php'
 captcha_url = 'http://158.69.76.135/captcha.php'
-windows_user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
-header = {'User-Agent': windows_user_agent,'Referer': URL
-}
+windows_user = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                "(KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36")
+header = {'User-Agent': windows_user, 'Referer': URL}
 api_key = '174faff8fbc769e94a5862391ecfd010'
 
 for i in range(5):
@@ -33,10 +33,13 @@ for i in range(5):
 
             site_key = response.cookies['HoldTheDoor']
 
-            my_data = {'id': '2282', 'key': site_key,'captcha': captcha,'holdthedoor': 'submit'}
+            my_data = {
+                'id': '2282', 'key': site_key,
+                'captcha': captcha, 'holdthedoor': 'submit'}
             # send data
             response_post = session.post(URL, headers=header, data=my_data)
-            print('Post Request made with status: {}'.format(response_post.status_code))
+            print('Status: {}'.format(response_post.status_code))
     except Exception as error:
         print(error)
         i -= 1
+print("Cheat online voting process finished :)")
